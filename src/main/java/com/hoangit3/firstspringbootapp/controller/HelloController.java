f7dac6d02d71fd5742d57f925021c02ca2426365
LUONG BA HOANG <hoang2kthptlk@gmail.com>
2022-08-13 17:34:38 +0700

    Update API

--- src/main/java/com/hoangit3/firstspringbootapp/controller/HelloController.java
+++ src/main/java/com/hoangit3/firstspringbootapp/controller/HelloController.java
@@ -9,11 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping(path = "hello")
     public String sayHello() {
-        return "Hello Hoang Anh";
+        return "Hello Kubernetes";
     }
 
     @GetMapping(path = "goodbye")
     public String sayGoodbye() {
-        return "Goodbye Anh Hoang";
+        return "Goodbye Kubernetes";
     }
 }
